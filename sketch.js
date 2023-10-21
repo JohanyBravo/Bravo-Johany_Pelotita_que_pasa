e2459571e4e49c4fce95b23ca81424b620d5b2f6
Johany Bravo <mayli.bravo@ucuenca.edu.ec>
2023-10-20 20:07:58 -0500

    Se agregó random en el color de las pelotas

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let posX;
+let posY;
+let diametro = 100;
+let radio = diametro / 2;
+let colorDeFondo;
+let vel = 10;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	posX = windowWidth / 2;
+	posY = windowHeight / 2;
+	// frameRate(10);
+	colorDeFondo = color(255, 200, 50);
+}
+
+function draw() {
+	background(colorDeFondo);
+	posX -= vel;
+
+	if (posX < -radio) {
+		diametro = random(10, 100);
+		radio = diametro / 2;
+		colorDeFondo = color(255, random(100, 255), 50);
+		// colorDeFondo.saturation(0.7);
+		posX = windowWidth + radio;
+		fill(0, random(-100, 255), 20);
+	}
+	circle(posX, posY, diametro);
+	print(posX);
+	
+}
